97fa12e084f97c9a7c876cd392abf6d1698c1061
Corvin Deboeser <corvin.deboeser@gmx.de>
2018-09-24 02:09:13 +0200

    Added theme and theme provider, formatted code, created app css classes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+
 import "./App.css";
+
+// A custom-defined color must have a 500 property
+const random = {
+  500: "#6A65F1"
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: random,
+    secondary: blue
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React App Main Component</h1>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className="App" id="app">
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
