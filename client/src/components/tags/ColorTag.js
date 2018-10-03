458f36c5f732299210aad6cc2fa1f08fe4bc5019
Corvin Deboeser <corvin.deboeser@gmx.de>
2018-10-03 02:55:53 +0200

    Changed ColorTag to rcc, implemented edit and delete,

--- client/src/components/tags/ColorTag.js
+++ client/src/components/tags/ColorTag.js
@@ -1,15 +1,136 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+import { connect } from "react-redux";
+import { compose } from "redux";
 
-const ColorTag = props => {
-  return (
-    <div>
-      <span>{props.tagname}</span>
-      <br />
-      <span>{props.id}</span>
-      <br />
-      <br />
-    </div>
-  );
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import Chip from "@material-ui/core/Chip";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+import standardTagConfiguration from "../common/standardTagConfiguration";
+import { triggerSnack } from "../../actions/snackActions";
+import { loadUserTags, deleteTag, setTag } from "../../actions/tagActions";
+
+const styles = theme => ({
+  card: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    margin: theme.spacing.unit * 0,
+    display: "inline-block",
+    marginBottom: 48
+  },
+  chip: {
+    margin: theme.spacing.unit * 0.25
+  },
+  exampleWrapper: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2
+  },
+  example: {},
+  media: {
+    height: 140
+  },
+  cardAction: {
+    position: "absolute",
+    bottom: 0
+  },
+  ...standardTagConfiguration(theme)
+});
+
+class ColorTag extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onDeleteClick = () => {
+    this.props.deleteTag(this.props.id, () => {
+      this.props.loadUserTags();
+      this.props.triggerSnack({
+        type: "success",
+        msg: "Tag was deleted successfully",
+        duration: 4000
+      });
+    });
+  };
+
+  onEditClick = () => {
+    this.props.setTag(this.props.tag);
+    this.props.handleClickOpen();
+  };
+
+  render() {
+    const { classes, tag, id } = this.props;
+
+    const textHightlightClasses = classNames(
+      classes.example,
+      classes[tag.background + "Light"],
+      classes[tag.color],
+      tag.underlined && classes.textUnderlined,
+      tag.bold && classes.textBold,
+      tag.bigger && classes.textBigger
+    );
+
+    const phraseChips = tag.phrases.map((phrase, key) => (
+      <Chip label={phrase} key={key} className={classes.chip} />
+    ));
+
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              {tag.tagname}
+            </Typography>
+            <Typography className={classes.exampleWrapper}>
+              This is an <span className={textHightlightClasses}>example</span>{" "}
+              text that shows how this color text looks like in a plain text{" "}
+              <span className={textHightlightClasses}>example</span>.
+            </Typography>
+            {phraseChips}
+          </CardContent>
+          <CardActions className={classes.cardAction}>
+            <Button size="small" color="primary" onClick={this.onDeleteClick}>
+              Delete
+            </Button>
+            <Button size="small" color="primary" onClick={this.onEditClick}>
+              Edit
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    );
+  }
+}
+
+ColorTag.propTypes = {
+  classes: PropTypes.object.isRequired,
+  tag: PropTypes.object.isRequired,
+  id: PropTypes.string.isRequired,
+  handleClickOpen: PropTypes.func.isRequired,
+  loadUserTags: PropTypes.func.isRequired,
+  deleteTag: PropTypes.func.isRequired,
+  setTag: PropTypes.func.isRequired,
+  triggerSnack: PropTypes.func.isRequired
 };
 
-export default ColorTag;
+const mapDispatchToProps = {
+  loadUserTags,
+  deleteTag,
+  setTag,
+  triggerSnack
+};
+
+export default compose(
+  withStyles(styles, { withTheme: true }),
+  connect(
+    null,
+    mapDispatchToProps
+  )
+)(ColorTag);
--- client/src/components/tags/ColorTags.js
+++ client/src/components/tags/ColorTags.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { compose } from "redux";
+import { withStyles } from "@material-ui/core/styles";
+
 import PropTypes from "prop-types";
 
-import { loadUserTags } from "../../actions/tagActions";
+import { loadUserTags, setTag } from "../../actions/tagActions";
 
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 import ColorTag from "./ColorTag";
+import CreateTagDialog from "./CreateTagDialog";
+import { colorClasses } from "../common/colors";
+
+const styles = theme => ({
+  grid: {
+    marginTop: theme.spacing.unit * 2
+  }
+});
 
 class ColorTags extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      dialogOpen: false,
       loading: true,
       tags: []
     };
@@ -32,20 +47,39 @@ class ColorTags extends Component {
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({ dialogOpen: true });
+  };
+
+  handleClose = () => {
+    this.setState({ dialogOpen: false });
+    this.props.setTag({});
+  };
+
   render() {
     const { tags, loading } = this.state;
+    const { classes } = this.props;
 
     let colorTags;
 
     if (loading) {
-      colorTags = <span>Loading...</span>;
+      colorTags = <LinearProgress />;
     } else {
       if (tags.length === 0) {
         colorTags = <span>You do not have any tags yet!! </span>;
       } else {
-        colorTags = tags.map(tag => (
-          <ColorTag key={tag._id} tagname={tag.tagname} id={tag._id} />
-        ));
+        colorTags = (
+          <Grid container className={classes.grid} spacing={16}>
+            {tags.map(tag => (
+              <ColorTag
+                key={tag._id}
+                id={tag._id}
+                tag={tag}
+                handleClickOpen={this.handleClickOpen}
+              />
+            ))}
+          </Grid>
+        );
       }
     }
 
@@ -53,6 +87,16 @@ class ColorTags extends Component {
       <div>
         <Typography variant="display1" gutterBottom>
           Your Color Tags
+        </Typography>
+        <Button onClick={this.handleClickOpen}>New Tag</Button>
+        <CreateTagDialog
+          state={this.state.dialogOpen}
+          handleClose={this.handleClose}
+        />
+        <Typography gutterBottom>
+          These are your color tags. When importing abstracts or papers, onverio
+          will highlight the key phrases that you define in each tag according
+          to the format of the tag.
         </Typography>
         {colorTags}
       </div>
@@ -70,10 +114,14 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  loadUserTags
+  loadUserTags,
+  setTag
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+export default compose(
+  withStyles(styles, { withTheme: true }),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
 )(ColorTags);
