2523c4fb1110e0c541e6886a46086fbfdf7560f4
Corvin Deboeser <corvin.deboeser@gmx.de>
2018-10-12 05:10:21 +0200

    Implemented section to retreive URL and abstract for DOI

--- client/src/components/query/SingleAbstract.js
+++ client/src/components/query/SingleAbstract.js
@@ -5,14 +5,17 @@ import { compose } from "redux";
 import { withStyles } from "@material-ui/core/styles";
 import { loadUserTags } from "../../actions/tagActions";
 import classNames from "classnames";
+import axios from "axios";
 
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-import { applyTags } from "./applyTags";
+import { applyTags } from "../../utils/applyTags";
 
 import PageActionHeadline from "../common/PageActionHeadline";
 import isEmpty from "../../validation/is-empty";
@@ -22,13 +25,31 @@ const styles = theme => ({
   root: {
     flexGrow: 1
   },
+  doicontrols: {
+    display: "flex"
+  },
+  doifield: {
+    flex: "auto"
+  },
+  doibuttons: {
+    flex: "initial"
+  },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: "center",
+    textAlign: "left",
     color: theme.palette.text.secondary
+  },
+  divider: {
+    marginBottom: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit * 2
   },
   btn: {
     marginTop: theme.spacing.unit
+  },
+  btnInline: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginTop: "auto"
   },
   ...standardTagConfiguration(theme)
 });
@@ -39,7 +60,14 @@ class SingleAbstract extends Component {
 
     this.state = {
       highlightText: "",
-      textSplit: []
+      doi: "",
+      doiError: "",
+      hyperlink: "",
+      hyperlinkLoading: false,
+      abstract: "",
+      abstractLoading: false,
+      manualAbstractSplit: [],
+      retrievedAbstractSplit: []
     };
   }
 
@@ -54,29 +82,119 @@ class SingleAbstract extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onTransferClick = text => {
+  onTransferClick = (target, text) => {
     applyTags(text, this.props.tags.appliedTags)
-      .then(res => this.setState({ textSplit: res }))
+      .then(res => this.setState({ [target]: res }))
       .catch(err => console.log("TODO: Handle error: ", err));
+  };
+
+  retrieveLink = () => {
+    if (isEmpty(this.state.doi)) {
+      this.setState({ doiError: "Please enter a DOI" });
+    } else {
+      this.setState({ doiError: "", hyperlinkLoading: true });
+
+      axios
+        .get(`/api/retrieve/doitourl/${this.state.doi}`)
+        .then(res => {
+          this.setState({
+            hyperlinkLoading: false,
+            hyperlink: res.data.link
+          });
+        })
+        .catch(err => {
+          this.setState({
+            hyperlinkLoading: false,
+            hyperlink: "Link could not be retrieved"
+          });
+        });
+    }
+  };
+
+  retrieveAbstract = () => {
+    if (isEmpty(this.state.doi)) {
+      this.setState({ doiError: "Please enter a DOI" });
+    } else {
+      this.setState({ abstractLoading: true, hyperlinkLoading: true });
+      axios
+        .get(`/api/retrieve/doitoabstract/${this.state.doi}`)
+        .then(res => {
+          this.setState({
+            abstractLoading: false,
+            hyperlinkLoading: false,
+            hyperlink: res.data.link,
+            abstract: res.data.abstract
+          });
+          this.onTransferClick("retrievedAbstractSplit", res.data.abstract);
+        })
+        .catch(err => {
+          this.setState({
+            abstractLoading: false,
+            hyperlinkLoading: false,
+            abstract: "Abstract could not be found"
+          });
+        });
+    }
   };
 
   render() {
     const { classes } = this.props;
+    const { doiError } = this.state;
     let text;
+    let hyperlink;
+    let abstract;
+    let manualAbstract;
+    let retrievedAbstract;
 
-    const htmlresult = this.state.textSplit.map((item, key) => {
-      let textHightlightClasses;
-      if (!isEmpty(item.classes)) {
-        textHightlightClasses = classNames(
-          ...item.classes.split(" ").map(item => classes[item])
+    if (!isEmpty(this.state.manualAbstractSplit)) {
+      manualAbstract = this.state.manualAbstractSplit.map((item, key) => {
+        let textHightlightClasses;
+        if (!isEmpty(item.classes)) {
+          textHightlightClasses = classNames(
+            ...item.classes.split(" ").map(item => classes[item])
+          );
+        }
+        return (
+          <span key={key} className={textHightlightClasses}>
+            {item.text}
+          </span>
         );
+      });
+    }
+
+    if (this.state.hyperlinkLoading) {
+      hyperlink = <LinearProgress />;
+    } else if (!isEmpty(this.state.hyperlink)) {
+      hyperlink = <Typography>{this.state.hyperlink}</Typography>;
+    } else {
+      hyperlink = <Typography>No link retrieved yet</Typography>;
+    }
+
+    if (this.state.abstractLoading) {
+      abstract = <LinearProgress />;
+    } else if (!isEmpty(this.state.retrievedAbstractSplit)) {
+      if (!isEmpty(this.state.retrievedAbstractSplit)) {
+        retrievedAbstract = this.state.retrievedAbstractSplit.map(
+          (item, key) => {
+            let textHightlightClasses;
+            if (!isEmpty(item.classes)) {
+              textHightlightClasses = classNames(
+                ...item.classes.split(" ").map(item => classes[item])
+              );
+            }
+            return (
+              <span key={key} className={textHightlightClasses}>
+                {item.text}
+              </span>
+            );
+          }
+        );
+        console.log(retrievedAbstract);
       }
-      return (
-        <span key={key} className={textHightlightClasses}>
-          {item.text}
-        </span>
-      );
-    });
+      abstract = <Typography>{retrievedAbstract}</Typography>;
+    } else {
+      abstract = <Typography>No abstract retrieved yet</Typography>;
+    }
 
     if (isEmpty(this.state.highlightText)) {
       text = "Enter some text dude";
@@ -102,7 +220,7 @@ class SingleAbstract extends Component {
               <Button
                 className={classes.btn}
                 onClick={() => {
-                  this.onTransferClick(text);
+                  this.onTransferClick("manualAbstractSplit", text);
                 }}
                 color="primary"
                 variant="contained"
@@ -114,7 +232,57 @@ class SingleAbstract extends Component {
           </Grid>
           <Grid item xs={12} sm={6} lg={9}>
             <Paper className={classes.paper}>
-              <Typography>{htmlresult}</Typography>
+              <Typography>{manualAbstract}</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <div className={classes.doicontrols}>
+                <div className={classes.doifield}>
+                  <TextField
+                    name="doi"
+                    label="DOI"
+                    value={this.state.doi}
+                    onChange={this.onChange.bind(this)}
+                    error={!isEmpty(doiError)}
+                    helperText={doiError}
+                    fullWidth
+                  />
+                </div>
+                <div className={classes.doibuttons}>
+                  <Button
+                    className={classes.btnInline}
+                    onClick={() => {
+                      this.retrieveLink();
+                    }}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Retrieve Link
+                  </Button>
+                  <Button
+                    className={classes.btnInline}
+                    onClick={() => {
+                      this.retrieveAbstract();
+                    }}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Retrieve Abstract
+                  </Button>
+                </div>
+              </div>
+
+              <Divider className={classes.divider} />
+              <Typography variant="headline" gutterBottom>
+                Hyperlink to Website
+              </Typography>
+              {hyperlink}
+              <Divider className={classes.divider} />
+              <Typography variant="headline" gutterBottom>
+                Abstract
+              </Typography>
+              {abstract}
             </Paper>
           </Grid>
         </Grid>
